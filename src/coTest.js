3c67005084bf5348cc6d37caccef8b466fbece3b
Yeltzin Salvador <blizzard.yeltzin@gmailcom>
2021-01-23 15:02:57 -0600

    update: split product update methods depending on its name; add Full Coverage and default update methods

--- src/coTest.js
+++ src/coTest.js
@@ -6,11 +6,34 @@ class Product {
   }
 
   updatePrice() {
-    if (this.price < 0 || isNaN(this.price)) { this.price = 0; }
-    if (!this.price) { return this; }
-    --this.price;
-    this.sellIn && --this.sellIn;
-    this.sellIn || --price;
+    return (this.updaters[this.name] || this.updaters.default)();
+  }
+
+  updaters = {
+    default: function() {
+      if (this.price < 0 || isNaN(this.price)) { this.price = 0; }
+      if (!this.price || !this.sellIn) { return this; }
+
+      --this.price;
+
+      this.sellIn && --this.sellIn;
+      this.sellIn || --price;
+
+      return this;
+    },
+
+    'Full Coverage': function() {
+      if (this.price < 0 || isNaN(this.price)) { this.price = 0; }
+      if (!this.sellIn) { return this; }
+
+      this.sellIn && --this.sellIn;
+
+      if (this.price > 50) { return this; }
+
+      ++this.price;
+
+      return this;
+    }
   }
 }
 
