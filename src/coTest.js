0791945765bdd525ba106083ecec9131f8786419
Yeltzin Salvador <blizzard.yeltzin@gmailcom>
2021-01-23 15:18:02 -0600

    add: runAfter30Days script; accept array of products or products data on Car Insurance constructor

--- src/coTest.js
+++ src/coTest.js
@@ -70,6 +70,7 @@ class Product {
 
 class CarInsurance {
   constructor(products = []) {
+    (products[0] && products[0] instanceof Product) || (products = products.map( product => new Product(product) ));
     this.products = products;
   }
 
